7355d28c26ab6053ddd729b4ded5022b36e0ed88
Martín Pena <52072219+mpenachayan@users.noreply.github.com>
2021-08-09 08:37:04 +0000

    [09.08.2021] Upload newly generated version of custom filters file for AdGuard Home #191 (#192)

--- adguard-release.sh
+++ adguard-release.sh
@@ -10,7 +10,7 @@ ISSUE_NUMBER=$(gh issue create --title "Make $CURRENT_DATE Release"  --body "Mak
 git checkout -b "release/adguard-filters-$CURRENT_DATE"
 git commit -m "[$CURRENT_DATE] Upload newly generated version of custom filters file for AdGuard Home #$ISSUE_NUMBER" 
 git push -u origin HEAD
-gh pr create --base mpenachayan:master --fill --assignee @me --title "[$CURRENT_DATE] Release preparation for day $(date +%d/%m/%Y)" --body "Release of Adguard Custom Filters generated for AdGuard Home to close issue #$ISSUE_NUMBER"
+gh pr create --base master --fill --assignee @me --title "[$CURRENT_DATE] Release preparation for day $(date +%d/%m/%Y)" --body "Release of Adguard Custom Filters generated for AdGuard Home to close issue #$ISSUE_NUMBER"
 gh pr merge --delete-branch --squash 
 gh release create $CURRENT_DATE --title "Release $CURRENT_DATE" --notes "Release of Adguard Custom Filters generated for AdGuard Home on day $CURRENT_DATE"
 gh issue close $ISSUE_NUMBER
