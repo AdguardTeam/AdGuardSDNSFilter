abc8fbbe0330bce23578bba83f9114000f883f9a
Martín Pena <52072219+mpenachayan@users.noreply.github.com>
2021-12-26 12:34:59 +0100

    #412 Allow empty commit during release process (#413)

--- adguard-release.sh
+++ adguard-release.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+ç#!/usr/bin/env bash
 
 CURRENT_DATE=$(date +%d.%m.%Y)
 
@@ -8,7 +8,7 @@ git pull upstream master
 git add . 
 ISSUE_NUMBER=$(gh issue create --title "Make $CURRENT_DATE Release"  --body "Make $CURRENT_DATE Release" --assignee @me --label release | sed 's|.*/||' )
 git checkout -b "release/adguard-filters-config-$CURRENT_DATE"
-git commit -m "[$CURRENT_DATE] Upload filters configuration files for AdGuard Home #$ISSUE_NUMBER" 
+git commit --allow-empty -m "[$CURRENT_DATE] Upload filters configuration files for AdGuard Home #$ISSUE_NUMBER" 
 git push -u origin HEAD
 gh pr create --base master --fill --assignee @me --title "[$CURRENT_DATE] Release preparation for day $(date +%d/%m/%Y)" --body "Release of Adguard Filters configuration for AdGuard Home to close issue #$ISSUE_NUMBER"
 gh pr merge --delete-branch --squash 
